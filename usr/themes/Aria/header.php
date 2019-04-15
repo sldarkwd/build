af452b31da845bc0111471cb4dbbf0404c787969
sldarkwd <qpepyks@foxmail.com>
2019-04-15 12:43:44 +0800

    Update header.php

--- usr/themes/Aria/header.php
+++ usr/themes/Aria/header.php
@@ -23,13 +23,13 @@
 
     <link rel="icon" type="image/ico" href="/favicon.ico">
 	<?php if(Utils::isEnabled('enableFancybox','AriaConfig')): ?>
-	<link href="<?php $this->options->themeUrl('assets/css/jquery.fancybox.min.css'); ?>" rel="stylesheet">
+	<link href="http://cdnth.sldark.xyz/usr/themes/aria/assets/css/jquery.fancybox.min.css" rel="stylesheet">
     <?php endif; ?>
-	<link href="<?php $this->options->themeUrl('assets/OwO/OwO.min.css'); ?>" rel="stylesheet">
-	<link href="<?php $this->options->themeUrl('assets/css/animate.min.css'); ?>" rel="stylesheet">
-    <link href="<?php $this->options->themeUrl('assets/css/iconfont.css'); ?>" rel="stylesheet" >
-    <link href="<?php $this->options->themeUrl('assets/css/style.min.css?v=213a50a4db'); ?>" rel="stylesheet">
-    <script src="<?php $this->options->themeUrl('assets/js/jquery.min.js'); ?>"></script>
+	<link href="http://cdnth.sldark.xyz/usr/themes/aria/assets/OwO/OwO.min.css" rel="stylesheet">
+	<link href="http://cdnth.sldark.xyz/usr/themes/aria/assets/css/animate.min.css" rel="stylesheet">
+    <link href="http://cdnth.sldark.xyz/usr/themes/aria/assets/css/iconfont.css" rel="stylesheet" >
+    <link href="http://cdnth.sldark.xyz/usr/themes/aria/assets/css/style.min.css?v=213a50a4db" rel="stylesheet">
+    <script src="http://cdnth.sldark.xyz/usr/themes/aria/assets/js/jquery.min.js"></script>
     <?php if($this->options->customHeader) $this->options->customHeader(); ?>
 	<!--[if lt IE 9]>
     <script src="http://cdn.staticfile.org/html5shiv/r29/html5.min.js"></script>
