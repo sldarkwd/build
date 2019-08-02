623e303cd8c4dae71823630c074f882513646334
sldarkwd <qpepyks@foxmail.com>
2019-08-03 01:02:02 +0800

    Update api.php

--- usr/plugins/l2d233/model/api.php
+++ usr/plugins/l2d233/model/api.php
@@ -5,19 +5,19 @@ if($_GET['p']==='22') $p = 2; else if($_GET['p']==='33') $p = 3; else $p = rand(
 if($p==2){$person = 22;$idle3 = 100;$tap1 = 150;$tap2 = 100;}else{$person = 33;$idle3 = 1000;$tap1 = 500;$tap2 = 200;}
 header("Content-type: application/json");
 if($_GET['model']==='rand') echo '{
-    "model":"'.$person.'/'.$person.'.v2.moc",
+    "model":"https://cdn.jsdelivr.net/gh/sldarkwd/pictures@a03cdf7/model/l2d233/model/'.$person.'/'.$person.'.v2.moc",
     "textures":[
-        "'.$person.'/textures/texture_00.png",
-        "'.$person.'/textures/texture_01/'.$ra1.'.png",
-        "'.$person.'/textures/texture_02/'.$ra1.'.png",
-        "'.$person.'/textures/texture_03/'.$ra2.'.png"
+        "https://cdn.jsdelivr.net/gh/sldarkwd/pictures@a03cdf7/model/l2d233/model/'.$person.'/textures/texture_00.png",
+        "https://cdn.jsdelivr.net/gh/sldarkwd/pictures@a03cdf7/model/l2d233/model/'.$person.'/textures/texture_01/'.$ra1.'.png",
+        "https://cdn.jsdelivr.net/gh/sldarkwd/pictures@a03cdf7/model/l2d233/model/'.$person.'/textures/texture_02/'.$ra1.'.png",
+        "https://cdn.jsdelivr.net/gh/sldarkwd/pictures@a03cdf7/model/l2d233/model/'.$person.'/textures/texture_03/'.$ra2.'.png"
 '; else echo '{
     "model":"./'.$person.'/'.$person.'.v2.moc",
     "textures":[
-        "'.$person.'/textures/texture_00.png",
-        "'.$person.'/textures/texture_01/1.png",
-        "'.$person.'/textures/texture_02/1.png",
-        "'.$person.'/textures/texture_03/11.png"
+        "https://cdn.jsdelivr.net/gh/sldarkwd/pictures@a03cdf7/model/l2d233/model/'.$person.'/textures/texture_00.png",
+        "https://cdn.jsdelivr.net/gh/sldarkwd/pictures@a03cdf7/model/l2d233/model/'.$person.'/textures/texture_01/1.png",
+        "https://cdn.jsdelivr.net/gh/sldarkwd/pictures@a03cdf7/model/l2d233/model/'.$person.'/textures/texture_02/1.png",
+        "https://cdn.jsdelivr.net/gh/sldarkwd/pictures@a03cdf7/model/l2d233/model/'.$person.'/textures/texture_03/11.png"
 '; echo '    ],
     "hit_areas_custom":{
         "head_x":[-0.35, 0.6],
@@ -34,31 +34,31 @@ if($_GET['model']==='rand') echo '{
     "motions":{
         "idle":[
             {
-                "file":"'.$person.'/'.$person.'.v2.idle-01.mtn",
+                "file":"https://cdn.jsdelivr.net/gh/sldarkwd/pictures@a03cdf7/model/l2d233/model/'.$person.'/'.$person.'.v2.idle-01.mtn",
                 "fade_in":2000,
                 "fade_out":2000
             },
             {
-                "file":"'.$person.'/'.$person.'.v2.idle-02.mtn",
+                "file":"https://cdn.jsdelivr.net/gh/sldarkwd/pictures@a03cdf7/model/l2d233/model/'.$person.'/'.$person.'.v2.idle-02.mtn",
                 "fade_in":2000,
                 "fade_out":2000
             },
             {
-                "file":"'.$person.'/'.$person.'.v2.idle-03.mtn",
+                "file":"https://cdn.jsdelivr.net/gh/sldarkwd/pictures@a03cdf7/model/l2d233/model/'.$person.'/'.$person.'.v2.idle-03.mtn",
                 "fade_in":'.$idle3.',
                 "fade_out":'.$idle3.'
             }
         ],
         "tap_body":[
             {
-                "file":"'.$person.'/'.$person.'.v2.touch.mtn",
+                "file":"https://cdn.jsdelivr.net/gh/sldarkwd/pictures@a03cdf7/model/l2d233/model/'.$person.'/'.$person.'.v2.touch.mtn",
                 "fade_in":'.$tap1.',
                 "fade_out":'.$tap2.'
             }
         ],
         "thanking":[
             {
-                "file":"'.$person.'/'.$person.'.v2.thanking.mtn",
+                "file":"https://cdn.jsdelivr.net/gh/sldarkwd/pictures@a03cdf7/model/l2d233/model/'.$person.'/'.$person.'.v2.thanking.mtn",
                 "fade_in":2000,
                 "fade_out":2000
             }
